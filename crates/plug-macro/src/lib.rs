62f32a1eae2b356dbd3adb4c6d4ae63c43a48c39
hazyfossa <hazyfossa@gmail.com>
2026-08-12 22:05:55 +0500

    Experimenting with proc-macros

--- crates/plug-macro/src/lib.rs
+++ crates/plug-macro/src/lib.rs
@@ -2,99 +2,118 @@
 
 #![allow(dead_code)]
 
-use proc_macro as raw;
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
+use quote::{ToTokens, quote};
 use syn::{
-    ItemImpl, ItemStruct, ItemTrait, Path, Result, Token,
-    parse::{Parse, ParseStream},
-    punctuated::Punctuated,
+    Field, Ident, ItemImpl, ItemStruct, ItemTrait, Path, Result, Token, punctuated::Punctuated,
     spanned::Spanned,
 };
 
 mod utils;
 use utils::*;
 
-#[proc_macro_attribute]
-pub fn plug(attrs: raw::TokenStream, input: raw::TokenStream) -> raw::TokenStream {
-    match plug_impl(attrs.into(), input.into()) {
-        Ok(tokens) => tokens.into(),
-        Err(e) => e.to_compile_error().into(),
+define!(plug = plug_impl);
+
+fn export(marker: &str, input: impl ToTokens) -> TokenStream {
+    let ident = Ident::new(marker, Span::call_site());
+
+    quote! {
+        #[doc(hidden)]
+        #[macro_export(local_inner_macros)]
+        macro_rules! #ident {
+            () => { #input };
+        }
+
+        pub(crate) use #ident;
     }
 }
 
+// TODO: figure out if this is useful or insane
+
+// struct WithPart<Mark, Other> {
+//     imported: TokenStream,
+//     other: Other,
+//     _mark: PhantomData<Mark>,
+// }
+
+// impl<Mark, Other> Deref for WithPart<Mark, Other> {
+//     type Target = Other;
+//     fn deref(&self) -> &Self::Target {
+//         &self.other
+//     }
+// }
+
+// impl<Mark, Other> Parse for WithPart<Mark, Other>
+// where
+//     Mark: Token + Parse,
+//     Other: Parse,
+// {
+//     fn parse(input: ParseStream) -> Result<Self> {
+//         input.parse::<Mark>()?; // TODO: context about parts here
+
+//         let imported;
+//         syn::bracketed!(imported in input);
+
+//         Ok(Self {
+//             imported: imported.parse()?,
+//             other: input.parse()?,
+//             _mark: PhantomData,
+//         })
+//     }
+// }
+
+#[derive(syn_derive::Parse)]
 enum Code {
+    #[parse(peek = Token![struct])]
     Struct(ItemStruct),
+    #[parse(peek = Token![trait])]
     Trait(ItemTrait),
+    #[parse(peek = Token![impl])]
     Impl(ItemImpl),
 }
 
-impl Parse for Code {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lookahead = input.lookahead1();
+// impl Parse for Code {
+//     fn parse(input: ParseStream) -> Result<Self> {
+//         let lookahead = input.lookahead1();
 
-        if lookahead.peek(syn::Token![struct]) {
-            input.parse().map(Self::Struct)
-        } else if lookahead.peek(syn::Token![trait]) {
-            input.parse().map(Self::Trait)
-        } else if lookahead.peek(syn::Token![impl]) {
-            input.parse().map(Self::Impl)
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
+//         if lookahead.peek(syn::Token![struct]) {
+//             input.parse().map(Self::Struct)
+//         } else if lookahead.peek(syn::Token![trait]) {
+//             input.parse().map(Self::Trait)
+//         } else if lookahead.peek(syn::Token![impl]) {
+//             input.parse().map(Self::Impl)
+//         } else {
+//             Err(lookahead.error())
+//         }
+//     }
+// }
 
-fn plug_impl(attrs: TokenStream, input: TokenStream) -> Result<TokenStream> {
-    let code: Code = syn::parse2(input)?;
-
-    match code {
+fn plug_impl(attrs: TokenStream, input: Code) -> Result<TokenStream> {
+    match input {
         Code::Trait(x) => trait_to_interface(attrs, x),
         Code::Struct(x) => struct_to_object(x),
         Code::Impl(x) => register_impl(x),
     }
 }
 
-#[derive(Debug)]
+#[derive(syn_derive::Parse, syn_derive::ToTokens)]
 enum Mode {
     Direct,
     FromMod,
 }
 
-#[derive(Debug)]
+#[derive(syn_derive::Parse, syn_derive::ToTokens)]
 struct ParsedAttrs {
     pub mode: Mode,
-    pub paths: Vec<Path>,
-}
 
-impl Parse for ParsedAttrs {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let ident: syn::Ident = input.parse()?;
-
-        let content;
-        syn::parenthesized!(content in input);
-
-        let paths: Vec<Path> = Punctuated::<Path, Token![,]>::parse_terminated(&content)?
-            .into_iter()
-            .collect();
-
-        ensure_empty!(input, "unexpected tokens after attribute arguments");
-
-        let mode = match ident.to_string().as_str() {
-            "direct" => Mode::Direct,
-            "from_mod" => Mode::FromMod,
-            other => {
-                bail!(ident, "expected `direct` or `from_mod`, found `{other}`");
-            }
-        };
-
-        Ok(ParsedAttrs { mode, paths })
-    }
+    #[parse(Punctuated::parse_terminated)]
+    pub paths: Punctuated<Path, Token![,]>,
 }
 
 impl ParsedAttrs {
     fn resolve_impls(self) -> Vec<Path> {
         match self.mode {
-            Mode::Direct => self.paths,
+            Mode::Direct => self.paths.into_iter().collect(),
             Mode::FromMod => todo!(),
         }
     }
@@ -109,13 +128,57 @@ fn trait_to_interface(attrs: TokenStream, input: ItemTrait) -> Result<TokenStrea
     let attrs: ParsedAttrs = syn::parse2(attrs)?;
     let impls = attrs.resolve_impls();
 
-    todo!()
+    let content = quote! {};
+
+    Ok(purescope(input.vis, input.ident, content))
 }
 
 fn struct_to_object(input: ItemStruct) -> Result<TokenStream> {
-    todo!()
+    let config: Vec<Field> = Vec::new();
+    let state: Vec<Field> = Vec::new();
+
+    let content = quote! {};
+
+    Ok(purescope(input.vis, input.ident, content))
 }
 
+const REGISTRATION_MARKER: &str = "__primary_object_for_this_module";
+
 fn register_impl(input: ItemImpl) -> Result<TokenStream> {
-    todo!()
+    let interface = match input.trait_ {
+        Some((path, _)) => path,
+        None => bail!(=> "This macro only makes sense for interface implementations"),
+    };
+
+    let content = quote! {};
+
+    Ok(content)
+}
+
+#[derive(syn_derive::Parse, syn_derive::ToTokens)]
+enum FnKind {
+    Regular,
+    Async,
+    Const,
+}
+
+#[derive(syn_derive::Parse, syn_derive::ToTokens)]
+struct FnShape {
+    name: Ident,
+    kind: FnKind,
+}
+
+#[derive(syn_derive::Parse, syn_derive::ToTokens)]
+struct MetaForInterface {
+    mode: Mode,
+
+    #[parse(Punctuated::parse_terminated)]
+    shape: Punctuated<FnShape, Token![,]>,
+}
+
+fn register_impl_inner(object: Ident, resolution_mode: Mode) -> TokenStream {
+    match resolution_mode {
+        Mode::Direct => TokenStream::new(),
+        Mode::FromMod => export(REGISTRATION_MARKER, object),
+    }
 }
--- crates/plug-macro/src/utils.rs
+++ crates/plug-macro/src/utils.rs
@@ -2,9 +2,61 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{Ident, Visibility};
 
+pub trait MacroReturn {
+    fn into_syn_result(self) -> syn::Result<TokenStream>;
+}
+
+impl MacroReturn for syn::Result<TokenStream> {
+    fn into_syn_result(self) -> syn::Result<TokenStream> {
+        self
+    }
+}
+
+impl MacroReturn for TokenStream {
+    fn into_syn_result(self) -> syn::Result<TokenStream> {
+        Ok(self)
+    }
+}
+
+// TODO: auto-define version for tests (syn::parse2)
+// TODO: infallible macros (no flatten)
+macro_rules! define {
+    ($(#[$($attr:tt)+])* $name:ident = $impl:ident) => {
+        $(#[$($attr)+])*
+        #[proc_macro_attribute]
+        pub fn $name(
+            attrs: proc_macro::TokenStream,
+            input: proc_macro::TokenStream,
+        ) -> proc_macro::TokenStream {
+            let predicate = |a,b| {
+                let a = syn::parse(a)?;
+                let b = syn::parse(b)?;
+                Ok((a, b))
+            };
+
+            let ret = predicate(attrs.into(), input.into())
+                .map(|(a,b)| $impl(a, b))
+                .map($crate::MacroReturn::into_syn_result)
+                .flatten();
+
+            match ret {
+                Ok(tokens) => tokens.into(),
+                Err(e) => e.to_compile_error().into(),
+            }
+        }
+    };
+}
+pub(crate) use define;
+
 macro_rules! bail {
-    ($tokens:expr, $($err:tt)*) => {
-        return Err(syn::Error::new($tokens.span(), format!($($err)*)))
+    (@span $tokens:expr) => { $tokens.span() };
+    (@span) => { proc_macro2::Span::call_site() };
+
+    ($($tokens:expr)? => $err:literal $($fmt:tt)*) => {
+        return Err(syn::Error::new(
+            $crate::bail!(@span $($tokens)?),
+            format!($err $($fmt)*)
+        ))
     };
 }
 pub(crate) use bail;
@@ -12,17 +64,90 @@ pub(crate) use bail;
 macro_rules! ensure_empty {
     ($tokens:expr, $($err:tt)*) => {
         if !$tokens.is_empty() {
-            bail!($tokens, $($err)*);
+            bail!($tokens => $($err)*);
         }
     };
 }
 pub(crate) use ensure_empty;
 
-pub fn purescope(vis: Visibility, name: Ident, content: TokenStream) -> TokenStream {
+pub fn purescope(vis: Visibility, ident: Ident, content: TokenStream) -> TokenStream {
     quote! {
-        #vis mod #name {
+        #[allow(non_snake_case)]
+        #vis mod #ident {
             pub use super::*;
             #content
         }
     }
 }
+
+macro_rules! tkpass {
+    (struct $name:ident {
+        $($field:ident: $type:ty),*
+        $(,)?
+    }) => {
+        struct $name {
+            $($field: $type),*
+        }
+
+        impl syn::parse::Parse for $name {
+            fn parse(input: ParseStream) -> Result<Self> {
+                $(
+                    let $field;
+                    syn::bracketed!($field in input);
+                    let $field = $field.parse()?;
+                )*
+
+                Ok(Self { $(
+                    $field,
+                )* })
+
+            }
+        }
+
+        impl quote::ToTokens for $name {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                $(
+                    syn::token::Bracket::default()
+                    .surround(tokens, |cx|
+                        self.$field.to_tokens(cx)
+                    );
+                )*
+
+            }
+        }
+    };
+
+    (enum $name:ident {
+        $($field:ident)*
+    }) => {
+        enum $name { $($field)* }
+
+        impl syn::parse::Parse for $name {
+            fn parse(input: ParseStream) -> Result<Self> {
+                $(
+                    let $field;
+                    syn::bracketed!($field in input);
+                    let $field = $field.parse()?;
+                )*
+
+                Ok(Self { $(
+                    $field,
+                )* })
+
+            }
+        }
+
+        impl quote::ToTokens for $name {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                $(
+                    syn::token::Bracket::default()
+                    .surround(tokens, |cx|
+                        self.$field.to_tokens(cx)
+                    );
+                )*
+
+            }
+        }
+    }
+}
+pub(crate) use tkpass;
